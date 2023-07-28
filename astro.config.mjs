7583259b72b7549abb3b00bcd2e8bef7fd57066c
Simon Vrachliotis <simon@simonswiss.com>
2023-07-28 16:16:35 +1000

    netlify adapter + package versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import markdoc from '@astrojs/markdoc';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc()],
   output: 'hybrid',
+  adapter: netlify()
 });
